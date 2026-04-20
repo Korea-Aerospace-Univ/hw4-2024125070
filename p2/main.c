53bf9761f47026a9eca5ede314192a079bc65bc4
2024125070 <holic020603@naver.com>
2026-04-20 16:44:34 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -3,39 +3,35 @@
 int main(void)
 {
     int n;      // 문자 개수
-    char str[100];      // 문자열 저장 배열
-    
     int eng_cur = 0, eng_max = 0;   // 연속된 소문자 영어 개수, 최대 연속 개수
     int num_cur = 0, num_max = 0;   // 연속된 숫자 개수, 최대 숫자 개수
     
     scanf("%d", &n);    // 문자 개수 입력
-    scanf("%s", str);   // 문자열 입력
+    getchar();  // 개행 제거(한 번)
     
     // 문자열 한 글자씩 확인
     for(int i = 0; i < n; i++) {
-        char c = str[i];    // 현재 문자
+        
+        char a = getchar();     // 한 글자씩 읽기
+        scanf("%c", &a);
         
         // 영어 소문자일 때
-        if (c >= 'a' && c <= 'z') {
+        if (a >= 'a' && a <= 'z') {
             eng_cur++;  // 연속 개수 증가
+            num_cur = 0;    // 끊기면 0 초기화
             // 최대값 갱신
             if (eng_cur > eng_max) {
                 eng_max = eng_cur;
             }
-        } 
-        else {
-            eng_cur = 0;    // 끊기면 0 초기화
         }
         
         // 숫자일 때
-        if (c >= '0' && c <= '9') {
-            num_cur++;  
+        else if (a >= '0' && a <= '9') {
+            num_cur++;
+            eng_cur = 0;
             if (num_cur > num_max) {
                 num_max = num_cur;
             }
-        }
-        else {
-            num_cur = 0;
         }
     }
     
