1351b9502e6d7f6eb5b57e2463c523a5b62abe6d
2024125070 <holic020603@naver.com>
2026-04-20 16:16:07 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -2,24 +2,26 @@
 
 int main(void)
 {
-    
-    int i = 0;
-    int a;
-    int b = 10;
+    int answer = 10;    // 정답
+    int guess = 0;    // 추측값
+    int count = 0;    // 시도 횟수
     
     do {
-        scanf("%d", &a);
-        i++;
+        scanf("%d", &guess);    // 정답 입력
+        count++;    // 시도 횟수 증가
         
-        if(a>b) {
-            printf("%d>?\n", a);
-        } else if(a<b){
-            printf("%d<?\n", a);
-        } else{
-            printf("%d==?\n", a);
-            printf("%d\n", i);
+        if(guess > answer) {
+            // 추측값이 정답 초과일 경우
+            printf("%d > ?\n", guess);
+        } else if(guess < answer){
+            // 추측값이 정답 미만일 경우
+            printf("%d < ?\n", guess);
+        } else {
+            // 추측값과 정답이 같을 경우
+            printf("%d == ?\n", guess);
+            printf("%d\n", count);
         }
-    } while(a!=b);
+    } while (guess != answer);    // 추측값이 정답과 같지 않다면 반복
 
     return 0;
 }
